66b087ca5f53a54f7cfac4e2fd600e8fa6b3dbe2
Hwigyeom <whistle96@gmail.com>
2021-11-03 01:24:54 +0900

    script 단위 테스트 실행 추가

---
+++ packages/application-shell/src/app.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './app';
+
+describe('App', () => {
+  const renderComponent = () => render(<App />);
+
+  it('랜더링 테스트', () => {});
+});
